216de47242133292aa34fd7d546f9888d518273b
Dnzhub <dnz.ylmz1@outlook.com>
2025-03-04 14:38:55 +0300

    Add UI for calculator

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+let number1 = 0;
+let number2 = 0;
+let selectedOperator = "";
+
 function add(...numbers)
 {
     return numbers.reduce((sum,number) => {
@@ -24,5 +28,25 @@ function divide(...numbers)
     return numbers.reduce((sum,number) => {
         return sum / number;
     })
+}
+
+function operate(operator, firstNum, lastNum)
+{
+    switch (operator) {
+        case '+':
+            add(firstNum,lastNum);
+            break;
+        case '-':
+            subtract(firstNum,lastNum);
+            break;
+        case '*':
+            multiply(firstNum,lastNum);
+            break;
+        case '/':
+            divide(firstNum,lastNum);
+            break;
+        default:
+            break;
+    }
 }
 
