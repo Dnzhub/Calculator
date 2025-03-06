25c79aa47de43ddaa0728cd8d7e3243f821a4b42
Dnzhub <dnz.ylmz1@outlook.com>
2025-03-06 12:43:46 +0300

    Finish v1 operator and numbers functionality

--- app.js
+++ app.js
@@ -1,52 +1,166 @@
-let number1 = 0;
-let number2 = 0;
-let selectedOperator = "";
+const INITIAL_VALUE = "0";
+const EMPTY_STRING = "";
+const MAX_NUMBER_LENGTH = 9;
 
-function add(...numbers)
-{
-    return numbers.reduce((sum,number) => {
+let number1 = "";
+let number2 = "";
+let selectedOperator = "";
+let isFirstNumber = true;
+let isInitialClick = true;
+
+
+
+const numbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const result = document.querySelector(".result");
+const clearButton = document.querySelector(".clearButton");
+const resultButton = document.querySelector(".resultButton");
+const decimalButton = document.querySelector(".decimalButton");
+
+function add(...numbers) {
+    return numbers.reduce((sum, number) => {
         return sum + number;
     })
 }
 
-function subtract(...numbers)
-{
-    return numbers.reduce((sum,number) => {
+function subtract(...numbers) {
+    return numbers.reduce((sum, number) => {
         return sum - number;
     })
 }
 
-function multiply(...numbers)
-{
-    return numbers.reduce((sum,number) => {
+function multiply(...numbers) {
+    return numbers.reduce((sum, number) => {
         return sum * number;
     })
 }
 
-function divide(...numbers)
-{
-    return numbers.reduce((sum,number) => {
+function divide(...numbers) {
+    return numbers.reduce((sum, number) => {
         return sum / number;
     })
 }
 
-function operate(operator, firstNum, lastNum)
-{
+function operate(operator, firstNum, lastNum) {
     switch (operator) {
         case '+':
-            add(firstNum,lastNum);
-            break;
+            return add(firstNum, lastNum);
         case '-':
-            subtract(firstNum,lastNum);
-            break;
+            return subtract(firstNum, lastNum);
         case '*':
-            multiply(firstNum,lastNum);
-            break;
+            return multiply(firstNum, lastNum);
         case '/':
-            divide(firstNum,lastNum);
-            break;
+            return divide(firstNum, lastNum);
         default:
-            break;
+            return add(firstNum, lastNum);
     }
 }
+
+
+
+
+function updateInputText() {
+    result.innerText = `${number1} ${selectedOperator} ${number2}`
+}
+
+function clearAllData() {
+    result.innerText = INITIAL_VALUE;
+    number1 = EMPTY_STRING;
+    number2 = EMPTY_STRING;
+    selectedOperator = EMPTY_STRING;
+    isInitialClick = true;
+    isFirstNumber = true;
+
+}
+
+function roundDecimal(num, decimalPlaces = 0) {
+    num = Math.round(num + "e" + decimalPlaces);
+    return Number(num + "e" + -decimalPlaces);
+}
+
+function strToNumber(num) {
+    let strNum = num.toString();
+    if (strNum.includes(".")) return parseFloat(roundDecimal(strNum, 4));
+    return parseInt(strNum);
+}
+
+//User can only use one decimal separator per number
+function shouldDisableDecimal(num) {
+    num.includes(".") ? decimalButton.disabled = true : decimalButton.disabled = false;
+}
+
+
+numbers.forEach(element => {
+    element.addEventListener("click", () => {
+        if (isInitialClick) {
+            clearAllData();
+            isInitialClick = false;
+        }
+        if (isFirstNumber && number1.length < MAX_NUMBER_LENGTH) {
+
+            number1 += element.innerText;
+            shouldDisableDecimal(number1);
+
+        }
+        else if (!isFirstNumber && number2.length < MAX_NUMBER_LENGTH) {
+            number2 += element.innerText;
+            shouldDisableDecimal(number2);
+
+
+        }
+        updateInputText();
+
+    })
+})
+
+
+
+operators.forEach(element => {
+    element.addEventListener("click", () => {
+        if (!number1) return;
+
+        isInitialClick = false;
+        if (number1 && number2) {
+            let newResult = operate(selectedOperator, strToNumber(number1), strToNumber(number2));
+            number1 = newResult;
+            number2 = "";
+            selectedOperator = element.innerText;
+            result.innerText = `${number1} ${selectedOperator}`;
+
+        }
+        else {
+            selectedOperator = element.innerText;
+            updateInputText();
+
+        }
+
+
+        isFirstNumber = false;
+    })
+})
+
+resultButton.addEventListener("click", () => {
+    if (!number1 || !selectedOperator || !number2) return;
+
+    if (selectedOperator == "/" && number2 == 0) {
+        alert(`You cant divide ${number1} to 0`);
+        clearAllData();
+        return;
+    }
+    let operationResult = operate(selectedOperator, strToNumber(number1), strToNumber(number2));
+    result.innerText = operationResult;
+    number1 = operationResult;
+    number2 = EMPTY_STRING;
+    selectedOperator = EMPTY_STRING;
+    isFirstNumber = true;
+    isInitialClick = true;
+
+})
+
+clearButton.addEventListener("click", clearAllData)
+
+window.addEventListener("load", () => {
+    result.innerText = INITIAL_VALUE;
+
+})
 
